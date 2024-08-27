c5cab316988565d0054a4ccdffa7a7c58a45ea04
PrinceTheDev <sinbadprince9@gmail.com>
2024-08-28 00:56:05 +0100

    fixing book search view

--- books/recommendation.py
+++ books/recommendation.py
@@ -91,9 +91,17 @@ def get_recommendations(book_id, books_data, tfidf_matrix):
     sim_scores = list(enumerate(cosine_sim[idx]))
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
 
-    book_indices = [i[0] for i in sim_scores[1:11]]  # Get top 10 recommendations
+    book_indices = [i[0] for i in sim_scores[1:11]]
 
-    return [books_data[i] for i in book_indices]
+    recommended_books = []
+    for i in book_indices:
+        try:
+            book = Book.objects.get(isbn=books_data[i]['isbn'])
+            recommended_books.append(book)
+        except Book.DoesNotExist:
+            continue
+
+    return recommended_books
 
 def recommend_books(user_id=None):
     """
