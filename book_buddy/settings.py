d29d956d30aa4727b3d198659439efef31848f48
PrinceTheDev <sinbadprince9@gmail.com>
2024-08-27 16:23:17 +0100

    tracking progress

--- book_buddy/settings.py
+++ book_buddy/settings.py
@@ -9,13 +9,15 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-import os
 from dotenv import load_dotenv
 from pathlib import Path
 from decouple import config
+import os
 
 
 load_dotenv()
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -25,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
-GOOGLE_BOOKS_API_KEY = os.getenv('GOOGLE_BOOK_API')
+GOOGLE_BOOKS_API_KEY = os.getenv('GOOGLE_BOOKS_API_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
