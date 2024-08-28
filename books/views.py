1a7b0b99a2ad2fad4c36984e2c8d92b27c29b74c
PrinceTheDev <sinbadprince9@gmail.com>
2024-08-28 01:31:36 +0100

    implementing basic frontend

--- books/views.py
+++ books/views.py
@@ -1,14 +1,28 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .recommendation import recommend_books, fetch_books_from_google, parse_book_data
 from .serializers import BookSerializer
-from rest_framework import viewsets
 from .models import Book
+from django.template.loader import render_to_string
 
+# Django Views
+def home_view(request):
+    return render(request, 'home.html')
 
-class BookViewSet(viewsets.ModelViewSet):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
+def book_detail_view(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    return render(request, 'book_detail.html', {'book': book})
+
+def search_results_view(request):
+    query = request.GET.get('query', '')
+    if not query:
+        return render(request, 'search_results.html', {'error': 'Query parameter is required'})
+    
+    items = fetch_books_from_google(query)
+    books = parse_book_data(items)
+    return render(request, 'search_results.html', {'books': books, 'query': query})
 
 @api_view(['GET'])
 def recommendations_view(request):
@@ -22,7 +36,7 @@ def search_books_view(request):
     query = request.query_params.get('query', '')
     if not query:
         return Response({'error': 'Query parameter is required'}, status=400)
-
+    
     items = fetch_books_from_google(query)
     books = parse_book_data(items)
     serializer = BookSerializer(books, many=True)
